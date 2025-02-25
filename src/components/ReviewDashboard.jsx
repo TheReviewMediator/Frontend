579a46f625fb7d851d4c686b60998400f9a12f2a
cheapcouches <lw05302021@gmail.com>
2025-02-24 18:06:31 -0600

    Refactored ReviewDashboard into a more object-oriented style

--- src/components/ReviewDashboard.jsx
+++ src/components/ReviewDashboard.jsx
@@ -1,45 +1,62 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import styles from './css/reviews.module.css';
 
+// The review itself - displays it's information and appropriate buttons
+// TODO - make the `...` show more options
+function Review({review}) {
+  return (
+    <div className={styles.reviewHeader}>
+      {review.platform}, <b>{review.rating} stars</b>
+      <div className={styles.reviewBody}>
+        {review.text}
+        <button className={styles.respondButton}>...</button>
+        <button className={styles.respondButton}>Respond</button>
+      </div>
+    </div>
+  )
+}
+
+// Builds a table of reviews
+// We do it like this because I think it's easier to format the HTML this way
+function ReviewTable({ reviews }) {
+  const rows = [];
+
+  reviews.forEach((review_item) => {
+    rows.push(
+      <Review review={review_item} />
+    );
+  })
+
+  return (
+    <table className={styles.reviewTable}>
+      <tbody> {rows} </tbody>
+    </table>
+  )
+}
+
+// The actual web page
 const ReviewDashboard = ({ businessId }) => {
     const [reviews, setReviews] = useState([]);
 
     useEffect(() => {
         const fetchReviews = async () => {
             try {
-                const response = await axios.get(`/api/reviews/getReviews?businessId=${businessId}`);
+                const response = await axios.get(process.env.BACKEND_URI + `/api/reviews/fetchReviews`);
+                console.log('Fetched reviews');
+                console.log(response);
                 setReviews(response.data);
             } catch (error) {
                 console.error('Error fetching reviews:', error);
             }
         };
-
         fetchReviews();
     }, [businessId]);
 
     return (
         <div>
-            <h2>Review Dashboard</h2>
-            <table>
-                <thead>
-                <tr>
-                    <th>Platform</th>
-                    <th>Rating</th>
-                    <th>Date</th>
-                    <th>Text</th>
-                </tr>
-                </thead>
-                <tbody>
-                {reviews.map((review) => (
-                    <tr key={review.id}>
-                        <td>{review.platform}</td>
-                        <td>{review.rating}</td>
-                        <td>{new Date(review.review_date).toLocaleDateString()}</td>
-                        <td>{review.review_text}</td>
-                    </tr>
-                ))}
-                </tbody>
-            </table>
+          <h2> Review Dashboard </h2>
+          <ReviewTable reviews={reviews}/>
         </div>
     );
 };
